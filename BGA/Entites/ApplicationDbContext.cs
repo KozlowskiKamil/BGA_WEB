b1d6931dc337bd033693c0a90e404a996db1aa4c
Kamil <kamil2k8@gmail.com>
2024-03-08 12:39:43 +0100

    działa - szukanie połączenia do bazydziała - szukanie jak uruchomić db

--- BGA/Entites/ApplicationDbContext.cs
+++ BGA/Entites/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql;
+
 
 namespace BGA.Entites
 {
@@ -6,11 +8,16 @@ namespace BGA.Entites
     {
         public DbSet<Repair> Repair { get; set; }
 
+
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Bga;Trusted_Connection=True");
-            optionsBuilder.UseSqlServer("Server=PLKWIM0SQLV02B\\ENG;Database=TE_CPK;Trusted_Connection=True");
-            //optionsBuilder.UseSqlServer("Server=PLKWIM0SQLV02B\\ENG;Database=TE_CPK;Integrated Security=False;User ID=neumannp;Password=;");
+            //optionsBuilder.UseSqlServer("Server=PLKWIM0SQLV02B\\ENG;Database=TE_CPK;Trusted_Connection=True");
+            //optionsBuilder.UseMySql("Server=mysql-kamil.alwaysdata.net;Database=kamil_printer;Integrated Security=True;User ID=kamil;Password=;"");
+            //optionsBuilder.UseSqlServer("Server=ysql-kamil.alwaysdata.net;Database=kamil_printer;Integrated Security=True;User ID=kamil;Password=;");       
+
+            optionsBuilder.UseSqlServer("Server=PLKWIM0SQLV02B\\ENG;Database=TE_CPK;Integrated Security=True;User ID=neumannp;Password=;");
             base.OnConfiguring(optionsBuilder);
         }
     }
