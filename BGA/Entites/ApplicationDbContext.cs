7910157b18fd826a44412845f926ba02ad55a901
Kamil <kamil2k8@gmail.com>
2024-04-08 13:25:07 +0200

    v1

--- BGA/Entites/ApplicationDbContext.cs
+++ BGA/Entites/ApplicationDbContext.cs
@@ -12,13 +12,9 @@ namespace BGA.Entites
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            //optionsBuilder.UseSqlServer("Server=PLKWIM0SQLV02B\\ENG;Database=TE_CPK;Trusted_Connection=True");
-            //optionsBuilder.UseMySql("Server=mysql-kamil.alwaysdata.net;Database=kamil_printer;Integrated Security=True;User ID=kamil;Password=;"");
-            //optionsBuilder.UseSqlServer("Server=ysql-kamil.alwaysdata.net;Database=kamil_printer;Integrated Security=True;User ID=kamil;Password=;");       
-            //optionsBuilder.UseSqlServer("Server=PLKWIM0SQLV02B\\ENG;Database=TE_CPK;Integrated Security=True;User ID=neumannp;Password=;");
-
-            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Bga;Trusted_Connection=True");
-            //optionsBuilder.UseSqlServer("Server = PLKWIM0SQLV02B\\ENG; Database = TE_CPK; Integrated Security = True; User ID = JABIL\\smckwi_bgaprd$");
+          
+            //optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Bga;Trusted_Connection=True");
+            optionsBuilder.UseSqlServer("Server = PLKWIM0SQLV02B\\ENG; Database = TE_CPK; Integrated Security = True; User ID = JABIL\\smckwi_bgaprd$");
             base.OnConfiguring(optionsBuilder);
         }
     }
